6853e5230f5f8fb41d4efdf35187e9a232463407
gratiscoin <bbbooor@aol.com>
2016-06-24 10:26:37 +0300

    global.php
    Greek translation

--- i8n/en/global.php
+++ i8n/en/global.php
@@ -3,15 +3,15 @@
 //Version: 0.0.1
 
 //Global : General
-$i8n['global_title']='Expanse | Open Blockchain Platform';
+$i8n['global_title']='Expanse | Ανοιχτή Πλατφόρμα Αλυσίδας Συναλλαγών';
 
 // Global : Navigation
-$i8n['nav_screenread']='Toggle navigation';
-$i8n['nav_slogan']='It\'s for everyone.';
-$i8n['nav_about']='About';
-$i8n['nav_downloads']='Downloads';
-$i8n['nav_documentation']='Documentation';
-$i8n['nav_team']='Team';
-$i8n['nav_community']='Community';
-$i8n['nav_explorer']='Block Explorer';
+$i8n['nav_screenread']='Εναλλαγή πλοήγησης';
+$i8n['nav_slogan']='Είναι για όλους.';
+$i8n['nav_about']='Σχετικά';
+$i8n['nav_downloads']='Λήψεις';
+$i8n['nav_documentation']='Τεκμηρίωση';
+$i8n['nav_team']='Ομάδα';
+$i8n['nav_community']='Κοινότητα';
+$i8n['nav_explorer']='Εξερευνητής Μπλόκ';
 ?>
